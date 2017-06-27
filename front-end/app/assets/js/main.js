307d4aa8a8f2a77ccba6bd5c630240d806885741
Jason Dumont <jason.dumont@sigma.se>
2017-06-27 10:34:22 +0100

    Added dynamic location

--- front-end/app/assets/js/main.js
+++ front-end/app/assets/js/main.js
@@ -3,7 +3,7 @@
  //Import socket io
 import io from 'socket.io-client';
 
-const socket = io.connect();
+const socket = io.connect(`${ location.protocol }//${ location.hostname }:${ location.port }`);
 
 //functions which need calling
 const Heart_Beat = require('./seperate/heart_beat'),
